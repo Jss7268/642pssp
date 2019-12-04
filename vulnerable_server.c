dcf9ed78a66d997e7bb2b1131e12e3563ca1d68e
Jack Smith <Jss7268@rit.edu>
2019-12-03 22:20:36 -0500

    Update vulnerable_server.c

--- vulnerable_server.c
+++ vulnerable_server.c
@@ -7,23 +7,27 @@
 #include<sys/wait.h> 
 #include <sys/signal.h>
 
-//#define PORT 8080 
 #define OK_STR ("OK\n")
 #define STACK_SMASH_STR ("SMASH\n")
 #define SEG_FAULT_STR ("SIGSEG\n")
 
+enum status{OK, SMASHED, SEG_FAULTED}; 
+
+// vulnerable method where stack buffer overflow can occur
+// if the socket receives too much data
 int overflow(int socket) {
 	char buffer[5];
 	unsigned int can0 = buffer[6];
 
-	// these print statements show how the canary value is static for SSP
-	// and dynamic for P-SSP
+	// this print statement show how the canary value is static for SSP and dynamic for P-SSP
 	printf("%d\n", can0);
 	int val = recv(socket, buffer, 1024, 0);
 	return val;
 }
 
-int createChild(int socket) {
+// this method spawns a child to "work" on the new connection.
+// if the child fails, then return a specific non-zero code.
+enum status createChild(int socket) {
 	pid_t cpid; 
 	int status;
 	if (fork()== 0) {
@@ -34,15 +38,16 @@ int createChild(int socket) {
 		if (WIFSIGNALED(status)) {
 			if (WTERMSIG(status) == SIGSEGV){
 				// on seg fault
-				return 1;
+				return SEG_FAULTED;
 			} else if (WTERMSIG(status) == SIGABRT) {
 				// on stack smashing detected
-				return 2;
+				return SMASHED;
 			}
 		}
-		return 0;
+		return OK;
 	}	
 }
+
 
 int main(int argc, char const *argv[]) { 
 	int serverfd, newSocket; 
@@ -56,8 +61,7 @@ int main(int argc, char const *argv[]) {
 	}
 	char *endptr;
 	long PORT = strtoll(argv[1], &endptr, 10);
-
-	   
+ 
 	// Creating socket file descriptor 
 	if ((serverfd = socket(AF_INET, SOCK_STREAM, 0)) == 0) { 
 		perror("socket failed"); 
@@ -83,24 +87,25 @@ int main(int argc, char const *argv[]) {
 		exit(EXIT_FAILURE); 
 	} 
 
+	// loop and wait for new connections
 	while (1) {
 		if ((newSocket = accept(serverfd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) { 
 			perror("accept"); 
 			exit(EXIT_FAILURE); 
 		}
 
-		int childStatus = createChild(newSocket);
+		enum status childStatus = createChild(newSocket);
 		
+		// send message depending on the status
 		char *message;
 		switch (childStatus) {
-			case 0:
+			case OK:
 				send(newSocket, OK_STR, strlen(OK_STR), 0);
 				break;
-			case 1:
+			case SEG_FAULTED:
 				send(newSocket, SEG_FAULT_STR, strlen(SEG_FAULT_STR), 0);
-				message = SEG_FAULT_STR;
 				break;
-			case 2:
+			case SMASHED:
 				// do nothing
 				break;
 		}
